35f741e376bdc7ab2bbf20cd9de129c3c8a43a6f
Verônica Bergelino <58296084+Veroberg@users.noreply.github.com>
2025-08-31 04:30:35 -0300

    Create 5_soma_usuario.py

---
+++ 5_soma_usuario.py
@@ -0,0 +1,14 @@
+# Atividade Prática 02 - Questão 5
+#
+# Leia 2 valores inteiros e armazene-os nas variáveis A e B.
+# Efetue a soma de A e B, atribuindo o seu resultado à variável X.
+# Entrada: A entrada contém 2 valores inteiros informados pelo usuário.
+# Saída: Imprima a mensagem "X = " (letra X maiúscula) seguido pelo valor da variável X e pelo final de linha.
+#
+# -----------------------------------------------------------------
+
+# Exemplo resolvido e Código interativo:
+A = int(input("Digite o valor de A: "))
+B = int(input("Digite o valor de B: "))
+X = A + B
+print(f"X = {X}")
